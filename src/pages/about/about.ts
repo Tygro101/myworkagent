16b9ffbd62be3e3ac683b89dc76cc82daf83544b
Ran Tayeb <Tayebran@gmail.com>
2018-08-27 14:41:25 +0300

    bug fix

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,34 +1,53 @@
-import { Component, OnInit } from '@angular/core';
-import { NavController } from 'ionic-angular';
-import { State } from '../../../node_modules/@ngrx/store';
-import { AppState, GeneralSetting, MonthWork, DayWork } from '../../store/state';
+import { Component, OnInit } from "@angular/core";
+import { NavController } from "ionic-angular";
+import { State, Store } from "../../../node_modules/@ngrx/store";
+import {
+  AppState,
+  GeneralSetting,
+  MonthWork,
+  DayWork,
+  CurrentState
+} from "../../store/state";
+import { getStateSelectore } from "../../store/selectors/selectors";
 
 @Component({
-  selector: 'page-about',
-  templateUrl: 'about.html'
+  selector: "page-about",
+  templateUrl: "about.html"
 })
-export class AboutPage implements OnInit{
-  private months:MonthWork[];
-  private days:DayWork[];
-  public monthsWrapper:MonthWorkWrapper[];
-  constructor(public navCtrl: NavController, private state: State<AppState>) {
-    
+export class AboutPage {
+  private months: MonthWork[];
+  private days: DayWork[];
+  public monthsWrapper: MonthWorkWrapper[];
+  private state: AppState;
+  constructor(
+    public navCtrl: NavController,
+    private store: Store<CurrentState>
+  ) {
+    store.select(getStateSelectore).subscribe((state: AppState) => {
+      this.state = state;
+      this.init();
+    });
   }
-  ngOnInit(): void {
-    this.months = Object.assign([], this.state.getValue().state.months);
-    this.days = Object.assign([], this.state.getValue().state.days);
-    this.papulateMonthWrapper();
+  init(): void {
+    if (this.state) {
+      this.months = Object.assign([], this.state.months);
+      this.days = Object.assign([], this.state.days);
+      this.papulateMonthWrapper();
+    }
   }
   papulateMonthWrapper(): void {
     this.monthsWrapper = [];
-    this.months.forEach((month:MonthWork)=>{
-      this.monthsWrapper.push({month:month, days:this.days.filter((day:DayWork)=> day.monthId == month.id)})
-    })
+    this.months.forEach((month: MonthWork) => {
+      this.monthsWrapper.push({
+        month: month,
+        days: this.days.filter((day: DayWork) => day.monthId == month.id)
+      });
+    });
     console.log(this.monthsWrapper);
   }
 }
 
-export interface MonthWorkWrapper{
-  month:MonthWork;
-  days:DayWork[]
+export interface MonthWorkWrapper {
+  month: MonthWork;
+  days: DayWork[];
 }
--- src/providers/get-date/get-date.ts
+++ src/providers/get-date/get-date.ts
@@ -10,8 +10,8 @@ export class GetDateProvider {
 
   public getNewDate():Date{
     var date = new Date();
-    //date.setMonth(date.getMonth()+ 1);
-    //date.setDate(date.getDate()+ 1);
+    date.setMonth(date.getMonth()+ 1);
+    date.setDate(date.getDate()+ 3);
     return date;
   }
 }
